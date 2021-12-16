7df456a42084d9e9c0680602bd4bfc2bffcd0c33
Min kyeong Tak <mtak0235@gmail.com>
2021-12-16 17:40:32 +0900

    test

--- srcs/requirements/wordpress/tools/install.sh
+++ srcs/requirements/wordpress/tools/install.sh
@@ -1,15 +1,12 @@
+#!bin/bash
 apt-get update
-apt-get upgrade -y
-apt-get install -y vim
-apt-get install -y unzip
-apt-get install -y wget
-apt-get install -y php-fpm php-mysql
-mkdir -p /var/www/html
-wget https://wordpress.org/latest.zip
-unzip latest.zip
-rm -rf latest.zip
-mv ./wp-config.php ./wordpress/
-mv ./www.conf /etc/php/7.3/fpm/pool.d/
-mv ./php-fpm.conf /etc/php/7.3/fpm/
-service php7.3-fpm start
-service php7.3-fpm stop
+apt-get upgrade -yq
+apt-get install -yq php7.3-fpm php7.3-mysql mariadb-client wget
+wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+chmod +x wp-cli.phar
+mv wp-cli.phar /usr/local/bin/wp
+mv ./www.conf /etc/php/7.3/fpm/pool.d/www.conf
+mkdir -p /var/www/wordpress
+chown -R  www-data:www-data /var/www/wordpress/
+RUN chmod 777 /var/www/wordpress/
+mkdir -p /run/php/
